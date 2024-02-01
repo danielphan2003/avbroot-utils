ccf2e782408e72bca6c37d1be7d826c57510dcbe
Daniel Phan <danielphan.2003@gmail.com>
2024-02-02 03:27:44 +0700

    feat: add target for the avb custom key setup process
    Signed-off-by: Daniel Phan <danielphan.2003@gmail.com>

--- scripts/setup_custom_avb_public_key.sh
+++ scripts/setup_custom_avb_public_key.sh
@@ -1,4 +1,21 @@
 #! /usr/bin/env bash
-fastboot erase avb_custom_key
-fastboot flash avb_custom_key ./avb_pkmd.bin
 
+PRJ_ROOT="$(pwd)"
+TARGET=""
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --target)
+            TARGET="$2"
+            shift 2
+            ;;
+    esac
+done
+
+if [ -z "$TARGET" ]; then
+    echo "Error: --target flag is required."
+    exit 1
+fi
+
+fastboot erase avb_custom_key
+fastboot flash avb_custom_key "$TARGET/avb_pkmd.bin"
